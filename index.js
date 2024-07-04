eed86c0038e449241cc9a8912164f069f72252a0
tgorsk15 <tgbusiness463@gmail.com>
2024-07-04 16:10:55 -0500

    take out log statements

--- index.js
+++ index.js
@@ -10,28 +10,24 @@ http.createServer(function (req, res) {
 
     if (req.url === '/') {
         fs.readFile(path.join(__dirname, "public", "index.html"), (err, data) => {
-            console.log('homePage found')
             if (err) throw err;
             res.writeHead(200, {'Content-Type': 'text/html'})
             res.end(data)
         })
     } else if(req.url === '/about') {
         fs.readFile(path.join(__dirname, "public", "about.html"), (err, data) => {
-            console.log('about page found')
             if (err) throw err;
             res.writeHead(200, {'Content-Type': 'text/html'})
             res.end(data)
         })
     } else if(req.url === '/contact-me') {
         fs.readFile(path.join(__dirname, "public", "contact-me.html"), (err, data) => {
-            console.log('Contact page found')
             if (err) throw err;
             res.writeHead(200, {'Content-Type': 'text/html'})
             res.end(data)
         })
     } else {
         fs.readFile(path.join(__dirname, "public", "404.html"), (err, data) => {
-            console.log('404 page')
             if (err) throw err;
             res.writeHead(200, {'Content-Type': 'text/html'})
             res.end(data)
