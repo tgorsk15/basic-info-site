3857511be7d78555d2e672cce453ea50ad3da123
tgorsk15 <tgbusiness463@gmail.com>
2024-07-04 16:09:44 -0500

    Add filepaths to about, contact, and 404 pages

--- index.js
+++ index.js
@@ -1,19 +1,37 @@
 import http from 'node:http';
-import url from 'node:url';
 import fs from 'node:fs';
-import EventEmitter from 'node:events';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-console.log(__dirname)
 
 http.createServer(function (req, res) {
-    console.log(req)
+
     if (req.url === '/') {
         fs.readFile(path.join(__dirname, "public", "index.html"), (err, data) => {
             console.log('homePage found')
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.end(data)
+        })
+    } else if(req.url === '/about') {
+        fs.readFile(path.join(__dirname, "public", "about.html"), (err, data) => {
+            console.log('about page found')
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.end(data)
+        })
+    } else if(req.url === '/contact-me') {
+        fs.readFile(path.join(__dirname, "public", "contact-me.html"), (err, data) => {
+            console.log('Contact page found')
+            if (err) throw err;
+            res.writeHead(200, {'Content-Type': 'text/html'})
+            res.end(data)
+        })
+    } else {
+        fs.readFile(path.join(__dirname, "public", "404.html"), (err, data) => {
+            console.log('404 page')
             if (err) throw err;
             res.writeHead(200, {'Content-Type': 'text/html'})
             res.end(data)
